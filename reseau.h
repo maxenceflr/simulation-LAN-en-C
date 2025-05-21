08322e3df1e122480f6fb0188996f908ed423444
Maxime Malys <malys@unistra.fr>
2025-05-21 16:38:19 +0200

    Changement implémentations et méthodes affichage

--- reseau.h
+++ reseau.h
@@ -1,23 +1,57 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef uint32_t adresseIP;
-typedef uint8_t adresseMAC[6];
+#define TAILLE_MAC_OCTETS 6
+#define TAILLE_IP_OCTETS 4
+#define TAILLE_MAX_ADRESSEMAC 500
+
+// Structure de donnée représentant une adresse MAC
+
+typedef struct {
+    uint64_t entier;
+    uint8_t bytes[TAILLE_MAC_OCTETS];
+} adresseMAC;
+
+// Structure de donnée représentant une adresse IP
+
+typedef struct {
+    uint32_t entier;
+    uint8_t bytes[TAILLE_IP_OCTETS];
+} adresseIP;
 
 // Structure de donnée représentant une station
 
-typedef struct station
+typedef struct Station
 {
-    adresseMAC adr_mac;
-    adresseIP adr_ip;
-}station;
+    adresseMAC adrMac;
+    adresseIP adrIP;
+}Station;
 
-// Structure de donnée représentant un switch
+// Structure de donnée représentant un port et les adresses MAC associées
 
-typedef struct s_switch
+typedef struct port
 {
-    adresseMAC adr_mac;
+    size_t num_port;
+    size_t nb_adressesMAC;
+    adresseMAC* tab_adresseMAC;
+}port;
+
+
+// Structure de donnée représentant un switch 
+
+typedef struct Switch
+{
+    adresseMAC adrMac;
     size_t nb_ports;
     size_t priorite;
+    port* tab_commutation;
+}Switch;  
 
-}s_switch;
+// Fonctions d'affichage
+
+void afficherIP(adresseIP adresse);
+void afficherMAC(adresseMAC adresse);
+void afficherTableCommutation(Switch sswitch);
+
+// Fonctions d'initialisation 
+
