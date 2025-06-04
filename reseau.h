409c1d832e6f5ab37f20b3f37d57e66d0dafe690
Maxime Malys <malys@unistra.fr>
2025-06-04 16:58:39 +0200

    Ajout des commentaires (avec une IA car flemme)

--- reseau.h
+++ reseau.h
@@ -8,58 +8,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Définitions des tailles pour les adresses
 #define TAILLE_MAC_OCTETS 6
 #define TAILLE_IP_OCTETS 4
 #define TAILLE_ADRESSE_MAX 500
 #define TAILLE_ADR_IP_STRING 16
 
-// Structure de donnée représentant une adresse MAC
-
+// Structure représentant une adresse MAC (6 octets)
 typedef struct adresseMAC
 {
     uint64_t entier;
     uint8_t bytes[TAILLE_MAC_OCTETS];
 } adresseMAC;
 
-// Structure de donnée représentant une adresse IP
-
+// Structure représentant une adresse IP (4 octets)
 typedef struct adresseIP
 {
     uint32_t entier;
     uint8_t bytes[TAILLE_IP_OCTETS];
 } adresseIP;
 
-// Structure de donnée représentant une station
-
+// Structure représentant une station (équipement terminal)
 typedef struct Station
 {
-    size_t id;
-    adresseMAC adrMAC;
-    adresseIP adrIP;
+    adresseIP adrIP;   // Adresse IP de la station
+    adresseMAC adrMac; // Adresse MAC de la station
+    size_t id;         // Identifiant unique de la station
 } Station;
 
-// Structure de donnée représentant un port et les adresses MAC associées
-
+// Structure représentant un port de switch
 typedef struct port
 {
     size_t nb_adressesMAC;
-    size_t adresses_capacite;
-    adresseMAC *tab_adresseMAC;
+    size_t adresses_capacite;   // Nombre d'adresses MAC connues sur ce port
+    adresseMAC *tab_adresseMAC; // Tableau dynamique d'adresses MAC connues sur ce port
 } port;
 
-// Structure de donnée représentant un switch
-
+// Structure représentant un switch réseau
 typedef struct Switch
 {
-    size_t id;
-    adresseMAC adrMac;
-    size_t nb_ports;
-    size_t priorite;
-    port *tab_commutation;
+    port *tab_commutation; // Table de commutation (tableau de ports)
+    adresseMAC adrMac;     // Adresse MAC du switch
+    size_t nb_ports;       // Nombre de ports
+    size_t priorite;       // Priorité STP
+    size_t id;             // Identifiant unique du switch
 } Switch;
 
 // Fonctions d'affichage
-
 void afficherIP(adresseIP adresse);
 void afficherMAC(adresseMAC adresse);
 void afficherSwitch(Switch sswitch);
@@ -68,18 +63,19 @@ void afficherPort(port r_port);
 void afficherTableCommutation(Switch sswitch);
 
 // Fonctions d'initialisation
-
+void deinit_switch(Switch *r_switch);
+void init_switch(Switch *r_switch, size_t nbPort, size_t numPriorite, adresseMAC adrMac);
 void init_station(Station *station, adresseIP adrIP, adresseMAC adrMAC);
 void init_port(port *r_port);
-void init_switch(Switch *r_switch, size_t nbPort, size_t numPriorite, adresseMAC adrMac);
-void deinit_switch(Switch *r_switch);
 void deinit_port(port *r_port);
 
-// Fonctions utilitaires
+// Fonctions de comparaison
+bool equals_port(port p1, port p2);
+bool equals_station(Station s1, Station s2);
+bool equals_switch(Switch s1, Switch s2);
+
+// Fonctions de gestion des ports
 
 void ajouter_adresse_port(port *port, adresseMAC adrMAC);
-bool equals_port(port p1, port p2);
-bool equals_switch(Switch s1, Switch s2);
-bool equals_station(Station s1, Station s2);
 
 #endif
